c2d40cffa61264e0b60a0c2fad2a029784e5341f
Abdelrahmen1313 <abdelrahmenbenfarhat@gmail.com>
2025-04-25 20:06:14 +0200

    adding pipes

--- src/utils/searchTermPipe.js
+++ src/utils/searchTermPipe.js
@@ -3,6 +3,13 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { sessionSearchTerms } from "..";
 
+/*
+This algorithm is provided in order to transport a tuple(array of strings) 
+from the main thread to the database and manage data transit for that tuple.
+*/
+
+// configs
+
 // Alternative for __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
