451ac92e5063af803000a1aaabd1903867ba30db
Dah <141691607+abdelrahmen1313@users.noreply.github.com>
2025-09-30 11:06:53 +0100

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -8,11 +8,14 @@ async function generateSearchTerm() {
     let searchTerms = await getSearchTerms(); // Get the search terms from the database
 
   const response = await ollama.generate({
+      
     model: "gemma3:1b",
-    prompt: `You are a data scientist shortcut and we are working on an app that fetch memes from the internet.You will generate search terms for memes based on the user's interests. Your mission is to guess a search term based from the user search history. The user has already searched for: ${searchTerms}.
-     The search term should be a single word or a short phrase. The search term should be relevant to the user's interests and should be something that could be used to find memes. The search term should be in English. The search term should not contain any special characters or numbers. The search term should not be too long or too short. The search term should not be too generic or too specific. The search term should not be offensive or inappropriate.`,
+    prompt:
+    `
+    if i tell you ${searchTerms} what comes to your mind ? 
+    `
     stream: false,
-    temperature: 0.5,
+    temperature: 0.1,
   });
   return response; // Return the generated search term
 }
